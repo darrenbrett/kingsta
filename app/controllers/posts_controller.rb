cf9b3739389ede4d2213e0bbcad65b3b3a0a05c2
Darren King <darrenbrettking@hotmail.com>
2016-01-18 22:32:51 -0800

    Created posts functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :find_posts, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to @post, notice: "Hey, Darren! Post has been added!"
+    else 
+      render 'new', notice: "Sorry, that didn't work, Darren."
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "Yeah! Post has been saved!"
+    else
+      render 'edit', notice: "Oops, try that again..."
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+  
+  def find_posts
+    @post = Post.find(params[:id])
+  end
+  
+end
