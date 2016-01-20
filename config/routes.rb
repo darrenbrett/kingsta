a289fbdb6330b9d8920eaf6eef42767ff4076e24
Darren King <darrenbrettking@hotmail.com>
2016-01-20 11:25:10 -0800

    Remove devise signup funciton

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   resources :posts
   resources :projects
   resources :contacts, only: [:new, :create]
@@ -11,5 +12,8 @@ Rails.application.routes.draw do
   match '/projects',  to: 'static_pages#projects',       via: 'get'
   match '/contact',   to: 'static_pages#contact',        via: 'get'
   match '/musing',    to: 'static_pages#musing/show',    via: 'get'
+  
+  
+  get '*path' => redirect('/')
 
 end
